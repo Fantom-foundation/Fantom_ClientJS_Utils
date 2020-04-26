4c62443623cd78f276ccd7a3e5058ef30db5b7fe
Jiri Malek <jirka.malek@gmail.com>
2020-04-26 12:26:01 +0200

    Test smart contract detection on Ledger device.

--- example/src/example.js
+++ example/src/example.js
@@ -94,7 +94,8 @@ async function getSignedTransaction(bridge) {
         gasLimit: "0xabe0",
         to: "0x76AE07E6D236c1aE3F5C3112F387ad82c69A2471", /* Sunstone */
         value: "0xde0b6b3a7640000", /* 1 FTM in WEI (1e+18) in HEX */
-        data: "0x506f6b7573", /* string Test */
+        // data: "0x506f6b7573", /* string Test */
+        data: "0xc312eb070000000000000000000000000000000000000000000000000000000000000003",
         chainId: "0xfa"
     };
 
