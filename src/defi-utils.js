c09c72d4e438d57338721506a58a045fa2cefb46
Jiri Malek <jirka.malek@gmail.com>
2020-07-21 20:54:08 +0200

    Add ERC20 allowance management transaction builders.

--- src/defi-utils.js
+++ src/defi-utils.js
@@ -257,7 +257,7 @@ function defiTradeTokenTx(liquidityPoolContract, fromTokenAddress, toTokenAddres
  *
  * @param {string} erc20Address
  * @param {string} recipientAddress
- * @param {string|{BN}} amount Amount to be transfered must be given in the token's decimals.
+ * @param {string|{BN}} amount Amount to be transferred must be given in the token's decimals.
  * @return {{gasLimit: string, data: string, chainId: string, to: string, nonce: undefined, value: string, gasPrice: undefined}}
  */
 function erc20TransferTx(erc20Address, recipientAddress, amount) {
@@ -297,6 +297,156 @@ function erc20TransferTx(erc20Address, recipientAddress, amount) {
             "stateMutability": "nonpayable",
             "type": "function"
         }, [recipientAddress, amount]),
+        chainId: OPERA_CHAIN_ID
+    };
+}
+
+/**
+ * erc20ApproveAmountTx creates a transaction for setting Allowance to the given
+ * amount for the ERC20 token and target contract/address.
+ *
+ * @param {string} erc20Address
+ * @param {string} delegatedToAddress
+ * @param {string|{BN}} amount
+ * @return {{gasLimit: string, data: string, chainId: string, to: string, nonce: undefined, value: string, gasPrice: undefined}}
+ */
+function erc20ApproveAmountTx(erc20Address, delegatedToAddress, amount) {
+    // create web3.js instance
+    const web3 = new Web3();
+
+    // make the transaction
+    return {
+        nonce: undefined,
+        gasPrice: undefined,
+        gasLimit: DEFAULT_GAS_LIMIT,
+        to: erc20Address,
+        value: ZERO_AMOUNT,
+        data: web3.eth.abi.encodeFunctionCall({
+            "constant": false,
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "spender",
+                    "type": "address"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "amount",
+                    "type": "uint256"
+                }
+            ],
+            "name": "approve",
+            "outputs": [
+                {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                }
+            ],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+        }, [delegatedToAddress, amount]),
+        chainId: OPERA_CHAIN_ID
+    };
+}
+
+/**
+ * erc20IncreaseAllowanceTx creates a transaction for increasing Allowance by the given
+ * amount for the ERC20 token and target contract/address.
+ *
+ * @param {string} erc20Address
+ * @param {string} delegatedToAddress
+ * @param {string|{BN}} addAmount
+ * @return {{gasLimit: string, data: string, chainId: string, to: string, nonce: undefined, value: string, gasPrice: undefined}}
+ */
+function erc20IncreaseAllowanceTx(erc20Address, delegatedToAddress, addAmount) {
+    // create web3.js instance
+    const web3 = new Web3();
+
+    // make the transaction
+    return {
+        nonce: undefined,
+        gasPrice: undefined,
+        gasLimit: DEFAULT_GAS_LIMIT,
+        to: erc20Address,
+        value: ZERO_AMOUNT,
+        data: web3.eth.abi.encodeFunctionCall({
+            "constant": false,
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "spender",
+                    "type": "address"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "addedValue",
+                    "type": "uint256"
+                }
+            ],
+            "name": "increaseAllowance",
+            "outputs": [
+                {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                }
+            ],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+        }, [delegatedToAddress, addAmount]),
+        chainId: OPERA_CHAIN_ID
+    };
+}
+
+/**
+ * erc20DecreaseAllowanceTx creates a transaction for decreasing Allowance by the given
+ * amount for the ERC20 token and target contract/address.
+ *
+ * @param {string} erc20Address
+ * @param {string} delegatedToAddress
+ * @param {string|{BN}} subAmount
+ * @return {{gasLimit: string, data: string, chainId: string, to: string, nonce: undefined, value: string, gasPrice: undefined}}
+ */
+function erc20DecreaseAllowanceTx(erc20Address, delegatedToAddress, subAmount) {
+    // create web3.js instance
+    const web3 = new Web3();
+
+    // make the transaction
+    return {
+        nonce: undefined,
+        gasPrice: undefined,
+        gasLimit: DEFAULT_GAS_LIMIT,
+        to: erc20Address,
+        value: ZERO_AMOUNT,
+        data: web3.eth.abi.encodeFunctionCall({
+            "constant": false,
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "spender",
+                    "type": "address"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "subtractedValue",
+                    "type": "uint256"
+                }
+            ],
+            "name": "decreaseAllowance",
+            "outputs": [
+                {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                }
+            ],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+        }, [delegatedToAddress, subAmount]),
         chainId: OPERA_CHAIN_ID
     };
 }
@@ -400,6 +550,9 @@ export default {
     defiRepayTokenTx,
     defiTradeTokenTx,
     erc20TransferTx,
+    erc20ApproveAmountTx,
+    erc20IncreaseAllowanceTx,
+    erc20DecreaseAllowanceTx,
     OPERA_CHAIN_ID,
     TESTNET_CHAIN_ID
 };
