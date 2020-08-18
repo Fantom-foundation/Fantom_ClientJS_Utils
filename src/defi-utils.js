dd660376093f1b5acedb7ef51789713167bc03c2
Jiri Malek <jirka.malek@gmail.com>
2020-08-18 18:01:15 +0200

    Add wrapped FTM tokens support.

--- src/defi-utils.js
+++ src/defi-utils.js
@@ -540,6 +540,88 @@ function defiRepayTokenTx(liquidityPoolContract, tokenAddress, amount) {
     };
 }
 
+/**
+ * defiWrapFtm creates a contract call transaction to wrap given amount
+ * of native FTM tokens into the wFTM tokens used among DeFi protocols.
+ *
+ * @param {string} erc20Address
+ * @param {string|{BN}} amount
+ * @returns {{gasLimit: string, data: string, chainId: string, to: *, nonce: undefined, value: string, gasPrice: undefined}}
+ */
+function defiWrapFtm(erc20Address, amount) {
+    // create web3.js instance
+    const web3 = new Web3();
+
+    // make the transaction
+    return {
+        nonce: undefined,
+        gasPrice: undefined,
+        gasLimit: DEFAULT_GAS_LIMIT,
+        to: erc20Address,
+        value: web3Utils.numberToHex(amount),
+        data: web3.eth.abi.encodeFunctionCall({
+            "constant": false,
+            "inputs": [],
+            "name": "deposit",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "payable": true,
+            "stateMutability": "payable",
+            "type": "function"
+        }, []),
+        chainId: OPERA_CHAIN_ID
+    };
+}
+
+/**
+ * defiWrapFtm creates a contract call transaction to unwrap given amount
+ * of wrapped wFTM tokens used among DeFi protocols back to the native FTM tokens.
+ *
+ * @param {string} erc20Address
+ * @param {string|{BN}} amount
+ * @returns {{gasLimit: string, data: string, chainId: string, to: *, nonce: undefined, value: string, gasPrice: undefined}}
+ */
+function defiUnwrapFtm(erc20Address, amount) {
+    // create web3.js instance
+    const web3 = new Web3();
+
+    // make the transaction
+    return {
+        nonce: undefined,
+        gasPrice: undefined,
+        gasLimit: DEFAULT_GAS_LIMIT,
+        to: erc20Address,
+        value: ZERO_AMOUNT,
+        data: web3.eth.abi.encodeFunctionCall({
+            "constant": false,
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "amount",
+                    "type": "uint256"
+                }
+            ],
+            "name": "withdraw",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+        }, [amount]),
+        chainId: OPERA_CHAIN_ID
+    };
+}
+
 // what we export here
 export default {
     defiDepositTokenTx,
@@ -549,6 +631,8 @@ export default {
     defiBorrowTokenTx,
     defiRepayTokenTx,
     defiTradeTokenTx,
+    defiWrapFtm,
+    defiUnwrapFtm,
     erc20TransferTx,
     erc20ApproveAmountTx,
     erc20IncreaseAllowanceTx,
